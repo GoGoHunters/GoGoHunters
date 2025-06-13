07297e5a52c2afac9736caae560e0d8d72eca2e9
may-04 <146904012+may-04@users.noreply.github.com>
2025-06-13 14:09:12 +0900

    Initial commit

---
+++ Source/GoGoHunters/GoGoHunters.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GoGoHunters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GoGoHunters, "GoGoHunters" );
+ 
