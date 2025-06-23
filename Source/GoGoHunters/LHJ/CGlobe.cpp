36bc67b416a1f0d52ac3ad54b3990cdcbc11b113
LHcom <55868914+LHcom@users.noreply.github.com>
2025-06-23 20:42:50 +0900

    폴더 정리, 지구본 클래스 추가

---
+++ Source/GoGoHunters/LHJ/CGlobe.cpp
@@ -0,0 +1,21 @@
+#include "CGlobe.h"
+
+
+ACGlobe::ACGlobe()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACGlobe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACGlobe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
