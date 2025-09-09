e0a1e3044151b6a9810549a1e6fbfdbb70e9bf34
LHcom <55868914+LHcom@users.noreply.github.com>
2025-09-10 00:43:54 +0900

    타미 위치 고정시키기

---
+++ Source/GoGoHunters/Private/LHJ/Trigger/CWorkingAreaTrigger.cpp
@@ -0,0 +1,91 @@
+#include "LHJ/Trigger/CWorkingAreaTrigger.h"
+
+#include "EngineUtils.h"
+#include "Components/BoxComponent.h"
+#include "JMH/MH_VRPlayer.h"
+#include "Utilities/CHelpers.h"
+
+ACWorkingAreaTrigger::ACWorkingAreaTrigger()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CHelpers::CreateComponent<UBoxComponent>(this, &BoxComponent, "BoxComponent");
+	CHelpers::CreateComponent<USceneComponent>(this, &AiLocSceneComponent, "AiLocSceneComponent", RootComponent);
+}
+
+void ACWorkingAreaTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (bActiveTrigger)
+	{
+		BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ACWorkingAreaTrigger::OnTriggerBeginOverrlap);
+		BoxComponent->OnComponentEndOverlap.AddDynamic(this, &ACWorkingAreaTrigger::OnTriggerEndOverrlap);
+
+		// Pawn 중에 Tami 태그 찾아서 저장
+		for (TActorIterator<APawn> It(GetWorld(), APawn::StaticClass()); It; ++It)
+		{
+			if (IsValid(*It) && (*It)->ActorHasTag(FName("Tami")))
+			{
+				TamiAI = *It;
+				break;
+			}
+		}
+	}
+}
+
+void ACWorkingAreaTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACWorkingAreaTrigger::OnTriggerBeginOverrlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 임시 비활성화 상태면 패스
+	if (bTemporaryDeactivation) return;
+	
+	// 플레이어가 아니면 패스
+	if (!OtherActor->IsA(AMH_VRPlayer::StaticClass())) return;
+
+	// 타미 없으면 패스
+	if (!TamiAI) return;
+	
+	FName FunctionName(TEXT("NotifyPlayerInTrigger"));
+	UFunction* Function = TamiAI->FindFunction(FunctionName);
+	if (Function)
+	{
+		FCNotifyPlayerInTrigger param;
+		param.bPlayerInTrigger = true;
+		param.TamiLocation = GetSceneCompLocation();
+		TamiAI->ProcessEvent(Function, &param);
+	}
+}
+
+void ACWorkingAreaTrigger::OnTriggerEndOverrlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// 임시 비활성화 상태면 패스
+	if (bTemporaryDeactivation) return;
+	
+	// 플레이어가 아니면 패스
+	if (!OtherActor->IsA(AMH_VRPlayer::StaticClass())) return;
+
+	// 타미 없으면 패스
+	if (!TamiAI) return;
+
+	FName FunctionName(TEXT("NotifyPlayerInTrigger"));
+	UFunction* Function = TamiAI->FindFunction(FunctionName);
+	if (Function)
+	{
+		FCNotifyPlayerInTrigger param;
+		param.bPlayerInTrigger = false;
+		TamiAI->ProcessEvent(Function, &param);
+	}
+}
+
+FVector ACWorkingAreaTrigger::GetSceneCompLocation()
+{
+	return AiLocSceneComponent->GetComponentLocation();
+}
